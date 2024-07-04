83fc55ec08dab3cd76ed2e52e437b9759c65f83c
beart75 <beart75@gmail.com>
2024-07-04 14:17:12 +0200

    ejercicios

---
+++ import sys.py
@@ -0,0 +1,11 @@
+#datos de la version del archivo python, 
+# # Librería estandar: sys - Es un módulo que permite manejar el interprete y extraer información de este.
+
+import sys
+
+data = sys.argv[1:]
+usuario, contraseña = data
+with open("contraseña.txt", "w") as archivo:
+    archivo.write(f"Usuario:    {usuario}\n")
+    archivo.write(f"contraseña:    {contraseña}\n")
+    print("se ha guardado todo")
