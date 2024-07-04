ba1f9504016924fa9bf2a2a8c34b78e2af8c7112
beart75 <beart75@gmail.com>
2024-07-04 19:45:53 +0200

    Corrección comprobacion numero romano

--- numeros_romanos.py
+++ numeros_romanos.py
@@ -4,16 +4,27 @@ romanos = {'I': 1, 'V': 5, 'X': 10,
 def roman2int(romval):
     total = 0
     prev = 0
+    contador = 0
+    
     for letra in romval[::-1]:
         valor = romanos[letra]
+        print(f"siguiente es {letra} = {valor} y anterior {prev} ")
         total += valor if valor >= prev else -valor
         prev = valor
-
-    return total
+        if (valor ==prev):
+            contador +=1
+    if contador > 4:
+        print("no es un numero romano valido")
+        return "ERROR"
+    else:
+        return total
 
 #tests = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "XIV", "CXC", "MIM"]
 
-test = input("Ingrese un numero romano: ")
+test = str.upper(input("Ingrese un numero romano: "))
+
 #comprobar que es un numero romano correcto
 # Si un símbolo romano está seguido de un símbolo mayor, réstale su valor al resultado.
+#FALTA ESTA COMPROBACION
+
 print(f"{test} es {roman2int(test)}")
