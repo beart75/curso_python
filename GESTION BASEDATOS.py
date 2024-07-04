aae9b8ac674f0208edf0f1c167a8ede6eb7bf51b
beart75 <beart75@gmail.com>
2024-07-04 19:57:24 +0200

    trabajando en el ejercicio de bbdd

--- GESTION BASEDATOS.py
+++ GESTION BASEDATOS.py
@@ -6,7 +6,7 @@
 #Validador de contraseña
 """
 
-
+SIN ACABAR
 
 """
 
@@ -20,15 +20,9 @@ class Usuario:
     def activarUsuario(self):
         print(f"¡{self.nombre} ha sido activado!")
 
-
-
-instancia = Usuario("Beart", "123ABC")
-
-
 ArchivoPw ="contraseña.txt"
 
-def creaContraseña():
-    pw = input("Introduce tu contraseña: ")
+def guardaContraseña(pw):
     archivo = open(ArchivoPw, "w")  # Modo de escritura
     archivo.write(pw)
     archivo.close() 
@@ -38,11 +32,59 @@ def leeContraseña():
     linea = archivo.readline()
     archivo.close() 
     return linea
+
+def ValidaPW(pw):
+    estado = True
+    isMayus = False
+    isMinus = False
+    isSpecial = False 
+    cadena ="!·$%&/()=?¿^*¨:;,@#\/+-'?¿[]{}" # CREAR UN DICCIONARIO ??
+    isNumber = False
+    if len(pw) < 12:
+        estado = False
+        print("contraseña de menos de 12 caracteres")
+    else:
+        for caracter in pw:
+            if caracter.isdigit():
+                isNumber = True
+            else:
+                isNumber = False
+                print("Sin numero")
+        for caracter in pw:
+            if caracter.isupper():
+                isMayus = True
+            else:
+                isMayus = False
+        print(f"{caracter} is  {isMayus}.")     
+        for caracter in pw:
+            if caracter.islower():
+                isMinus = True
+            else:
+                isMinus = False
+                print("Sin minuscula")
+        print(f"{caracter} is  {isMinus}.")
+        for caracter in pw:
+            for letra in cadena:
+                if pw.find(letra) > 0:
+                    isSpecial = True
+                    print("Tiene caracter especial.")
+        if isMayus == True and isMinus == True and isSpecial == True and isNumber == True:
+            estado = True
+        else:
+            estado = False
+    return estado        
+ 
+
 def CreaUsuario():
     usuario = input("Introduce tu nombre de usuario: ")
+    contraseña = input("Introduce tu contraseña ")
+    if ValidaPW(contraseña):
+        guardaContraseña(contraseña)
 
-creaContraseña()
+
+instancia = Usuario("Beart", "123ABC")
+
 print("CONTRASEÑA INTRODUCIDA")
-ln = leeContraseña()
+ln = leeContraseña() # si es la misma que la guardada , puedes pasar
 print(f"Tu contraseña es {ln}")
 
